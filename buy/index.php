f08a3d82b67a5129a55c707026e7429a21ad76ef
ecit010223 <ecit_eminem@163.com>
2017-08-20 11:35:43 +0800

    格式化

--- buy/index.php
+++ buy/index.php
@@ -15,13 +15,31 @@
 		$s_id=session_id(); 
 	?>
 		<table align="center">
-			<tr><th colspan="2">欢迎购买</th></tr>
-			<tr><td>天龙八部</td><td><a href="buyIt.php?book_id=sn1&book_name='天龙八部'&<?php echo SID;?>">点击购买</a></td></tr>
-			<tr><td>笑傲江湖</td><td><a href="buyIt.php?book_id=sn2&book_name='笑傲江湖'&<?php echo SID;?>">点击购买</a></td></tr>
-			<tr><td>射雕英雄传</td><td><a href="buyIt.php?book_id=sn3&book_name='射雕英雄传'&<?php echo SID;?>">点击购买</a></td></tr>
-			<tr><td>雪山飞孤</td><td><a href="buyIt.php?book_id=sn4&book_name='雪山飞孤'&<?php echo SID;?>">点击购买</a></td></tr>
-			<tr><td colspan="2"><hr/></td></tr>
-			<tr><td colspan="2"><a href="hasBuy.php?<?php echo SID;?>">查看您已经购买的商品列表</a></td></tr>
+			<tr>
+				<th colspan="2">欢迎购买</th>
+			</tr>
+			<tr>
+				<td>天龙八部</td>
+				<td><a href="buyIt.php?book_id=sn1&book_name='天龙八部'&<?php echo SID;?>">点击购买</a></td>
+			</tr>
+			<tr>
+				<td>笑傲江湖</td>
+				<td><a href="buyIt.php?book_id=sn2&book_name='笑傲江湖'&<?php echo SID;?>">点击购买</a></td>
+			</tr>
+			<tr>
+				<td>射雕英雄传</td>
+				<td><a href="buyIt.php?book_id=sn3&book_name='射雕英雄传'&<?php echo SID;?>">点击购买</a></td>
+			</tr>
+			<tr>
+				<td>雪山飞孤</td>
+				<td><a href="buyIt.php?book_id=sn4&book_name='雪山飞孤'&<?php echo SID;?>">点击购买</a></td>
+			</tr>
+			<tr>
+				<td colspan="2"><hr/></td>
+			</tr>
+			<tr>
+				<td colspan="2"><a href="hasBuy.php?<?php echo SID;?>">查看您已经购买的商品列表</a></td>
+			</tr>
 		</table>
 	</body>
 </html>
